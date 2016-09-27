6b6b1efd7a4903ee6b268e4cc0924445d54e03b4
Maxim Romanov <romanov.maxim@gmail.com>
2016-09-27 18:16:21 +0200

    Forms for double-checking ready

--- ara/book_IbnFaqihHamadhani.Buldan/doublecheck_viewer.py
+++ ara/book_IbnFaqihHamadhani.Buldan/doublecheck_viewer.py
@@ -8,7 +8,7 @@ htmlTop = """
 <html>
 
 <head>
-  <title>Creating GoldStandard</title>
+  <title>fileName</title>
 
   <meta charset="UTF-8">
   <meta name="description" content="Reviewing OCR Training/Testing Data">
@@ -20,8 +20,20 @@ htmlTop = """
     <style>
       body {background-color: white;}
       p {color: red; direction: rtl; font-size: 38pt; text-align: center; font-family: "Geeza Pro",  "Amiri"}
-      h1 {color: blue; font-size: 28pt; text-align: left}
+      h1 {color: darkgreen; font-size: 20pt; text-align: left; font-family: "Baskerville",  "Garamond"}
       img {height: 50pt; text-align: center}
+      #download_button {
+      position: fixed;
+      padding: 0;
+      text-align: left;
+      width: 15%;
+      bottom: 10px;
+      right: 20px;
+      font-size: 16pt;
+      color: #8B0000;
+      font-weight: bold;
+      }
+      
     </style>
 
 </head>
@@ -35,6 +47,8 @@ htmlBot = """
 
 </table>
 
+<a id="download_button" href="#" download>SAVE CORRECTIONS</a>
+
 </body>
 
 </html>
@@ -45,6 +59,7 @@ htmlBot = """
 def getText(fileName):
     with open(fileName[:-4]+'.gt.txt', "r", encoding="utf8") as f1:
         text = f1.read()
+        text = text.replace('"', "«»")
         #print(text)
         #text = list(text)
         #print(text)
@@ -69,29 +84,37 @@ def createHTMLs(lines):
     for f in files:
         if f.endswith(".png"):
             # <a href="file://C:/path/to/file/file.html">Link Anchor</a>
-            href = '<h1><a href="%s">%s</a></h1>'
-            head = href % ((folder+f[:-4]+'.gt.txt'), (f[:-4]+'.gt.txt'))
-            tags = head + '<p><img src="%s"><br>' % (folder+f)
-            text = tags+"%s</p>" % getText(folder+f)
+            href = '<h1>File: %s (if the image is defective, simply delete all Arabic text and the line will be excluded)</h1>'
+            head = href % (f[:-4]+'.gt.txt')
+            tags = head + '<p><img src="%s"></p>' % (folder+f)
+            text = tags+"<p contenteditable=\"true\" fileNameId=\"%s\">%s</p>" % ((f[:-4]+'.gt.txt'), getText(folder+f))
             table.append('<tr align="center">%s</tr><hr>' % text)
             counter += 1
             if counter % lines == 0:
-                with open("%s%06d.html" % (pref, counter), "w", encoding="utf8") as f9:
-                    f9.write(htmlTop + "\n\n".join(table) + htmlBot)
+                fileName = "%s%06d.html" % (pref, counter)
+                newHTML = htmlTop.replace(">fileName<", ">"+fileName[:-5]+"_corrected<")
+                with open(fileName, "w", encoding="utf8") as f9:
+                    f9.write(newHTML + "\n\n".join(table) + htmlBot)
                 table = []
                 print("%s%06d.html" % (pref, counter))
                 #input("Check!")
 
     newCount = roundup(counter, lines)
-    with open("%s%06d.html" % (pref, newCount), "w", encoding="utf8") as f9:
-        f9.write(htmlTop + "\n\n".join(table) + htmlBot)
-        print("%s%06d.html" % (pref, newCount))
+    fileName = "%s%06d.html" % (pref, newCount)
+    newHTML = htmlTop.replace(">fileName<", ">"+fileName[:-5]+"_corrected<")
+    with open(fileName, "w", encoding="utf8") as f9:
+        f9.write(newHTML + "\n\n".join(table) + htmlBot)    
+
         
 
-folder = "./5_goldStandard_b/"
-pref   = "gs_IbnFaqihBuldan_b_"
-#folder = os.path.abspath(folder)+"/"
-createHTMLs(100)    
+folder = "./5_goldStandard/"
+pref = (os.path.abspath(folder)).split("/")[-2].replace(".","_")
+pref = "gs_"+pref[5:]+"_"
+
+print(pref)
+print()
+
+createHTMLs(30)    
 
         
         
--- ara/book_Yacqubi.Tarikh/doublecheck_viewer.py
+++ ara/book_Yacqubi.Tarikh/doublecheck_viewer.py
@@ -8,7 +8,7 @@ htmlTop = """
 <html>
 
 <head>
-  <title>Creating GoldStandard</title>
+  <title>fileName</title>
 
   <meta charset="UTF-8">
   <meta name="description" content="Reviewing OCR Training/Testing Data">
@@ -19,9 +19,21 @@ htmlTop = """
   
     <style>
       body {background-color: white;}
-      p {color: red; direction: rtl; font-size: 38pt; text-align: center; font-family: "Geeza Pro", "Amiri"}
-      h1 {color: blue; font-size: 28pt; text-align: left}
+      p {color: red; direction: rtl; font-size: 38pt; text-align: center; font-family: "Geeza Pro",  "Amiri"}
+      h1 {color: darkgreen; font-size: 20pt; text-align: left; font-family: "Baskerville",  "Garamond"}
       img {height: 50pt; text-align: center}
+      #download_button {
+      position: fixed;
+      padding: 0;
+      text-align: left;
+      width: 15%;
+      bottom: 10px;
+      right: 20px;
+      font-size: 16pt;
+      color: #8B0000;
+      font-weight: bold;
+      }
+      
     </style>
 
 </head>
@@ -35,6 +47,8 @@ htmlBot = """
 
 </table>
 
+<a id="download_button" href="#" download="%s">SAVE CORRECTIONS</a>
+
 </body>
 
 </html>
@@ -45,6 +59,7 @@ htmlBot = """
 def getText(fileName):
     with open(fileName[:-4]+'.gt.txt', "r", encoding="utf8") as f1:
         text = f1.read()
+        text = text.replace('"', "«»")
         #print(text)
         #text = list(text)
         #print(text)
@@ -69,28 +84,39 @@ def createHTMLs(lines):
     for f in files:
         if f.endswith(".png"):
             # <a href="file://C:/path/to/file/file.html">Link Anchor</a>
-            href = '<h1><a href="%s">%s</a></h1>'
-            head = href % ((folder+f[:-4]+'.gt.txt'), (f[:-4]+'.gt.txt'))
-            tags = head + '<p><img src="%s"><br>' % (folder+f)
-            text = tags+"%s</p>" % getText(folder+f)
+            href = '<h1>File: %s (if the image is defective, simply delete all Arabic text and the line will be excluded)</h1>'
+            head = href % (f[:-4]+'.gt.txt')
+            tags = head + '<p><img src="%s"></p>' % (folder+f)
+            text = tags+"<p contenteditable=\"true\" fileNameId=\"%s\">%s</p>" % ((f[:-4]+'.gt.txt'), getText(folder+f))
             table.append('<tr align="center">%s</tr><hr>' % text)
             counter += 1
             if counter % lines == 0:
-                with open("gs_%06d.html" % counter, "w", encoding="utf8") as f9:
-                    f9.write(htmlTop + "\n\n".join(table) + htmlBot)
+                fileName = "%s%06d.html" % (pref, counter)
+                corr = fileName[:-5]+"_corrected.html"
+                newHTML = htmlTop.replace(">fileName<", ">"+fileName[:-5]+"_corrected.html<")
+                with open(fileName, "w", encoding="utf8") as f9:
+                    f9.write(newHTML + "\n\n".join(table) + htmlBot % corr)
                 table = []
-                print("\tgs_%06d.html" % counter)
+                print("%s%06d.html" % (pref, counter))
                 #input("Check!")
 
     newCount = roundup(counter, lines)
-    with open("gs_%06d.html" % newCount, "w", encoding="utf8") as f9:
-        f9.write(htmlTop + "\n\n".join(table) + htmlBot)
-        print("\tgs_%06d.html" % newCount)
+    fileName = "%s%06d.html" % (pref, newCount)
+    corr = fileName[:-5]+"_corrected.html"
+    newHTML = htmlTop.replace(">fileName<", ">"+fileName[:-5]+"_corrected.html<")
+    with open(fileName, "w", encoding="utf8") as f9:
+        f9.write(newHTML + "\n\n".join(table) + htmlBot % corr)
+
         
 
 folder = "./5_goldStandard/"
-#folder = os.path.abspath(folder)+"/"
-createHTMLs(100)    
+pref = (os.path.abspath(folder)).split("/")[-2].replace(".","_")
+pref = "gs_"+pref[5:]+"_"
+
+print(pref)
+print()
+
+createHTMLs(30)    
 
         
         
