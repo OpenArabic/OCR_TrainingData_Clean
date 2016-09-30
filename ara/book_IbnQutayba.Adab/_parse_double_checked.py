b4d6e7e66de40cc93132f3ec49b5c51081629736
Maxim Romanov <romanov.maxim@gmail.com>
2016-10-01 00:26:17 +0200

    more data

---
+++ ara/book_IbnQutayba.Adab/_parse_double_checked.py
@@ -0,0 +1,58 @@
+import os, shutil, re
+
+# filenames from the folder
+# parse out values from html
+# copy/save into a new folder
+# create a manifesto
+
+
+sFolder = "./6_DoubleChecked/"
+tFolder = "./7_final/"
+
+def parsing():
+    lof = os.listdir(sFolder)
+    allText = []
+
+    for f in lof:
+        if f.endswith(".html"):
+            print(f)
+            picFolder = f[:-5]+"_files/"
+            with open(sFolder+f, "r", encoding="utf8") as ft:
+                text = ft.read()
+                for l in re.findall("<p contenteditable=.*?</p>|<p filenameid=.*?</p>", text):
+                    l = re.sub("<br>|</?div>|</?span.*?>", "", l)
+                    l = re.sub("&nbsp;", " ", l)
+                    #print(l)
+                    fn = re.search('filenameid="(.*?)"', l).group(1)
+                    #print(fn)
+                    val = re.search(">(.*?)</p>", l).group(1)
+                    #print(val)
+                    #input()
+                    if val.strip() == "":
+                        #input()
+                        pass
+                    else:
+                        with open(tFolder+fn, "w", encoding="utf8") as ft2:
+                            ft2.write(val)
+                        imName = fn[:-7]+".png"
+                        shutil.copy2(sFolder+picFolder+imName, tFolder+imName)
+                        allText.append(fn+"\t"+val)
+    # create a manifest
+    man = os.listdir(tFolder)
+    manNew = []
+    for i in man:
+        if i.endswith(".png"):
+            manNew.append(i)
+    with open(tFolder+"manifest.txt", "w", encoding="utf8") as f9:
+        f9.write("\n".join(manNew))
+
+    with open("test.txt", "w", encoding="utf8") as ftest:
+        ftest.write("\n".join(allText))
+
+
+parsing()
+print("Done!")
+
+
+        
+        
